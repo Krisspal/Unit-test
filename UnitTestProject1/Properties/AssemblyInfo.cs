1e1f2978ad11d7959344ee038aef54e023987499
Kriss <69229024+Krisspal@users.noreply.github.com>
2023-11-10 12:37:00 +0700

    Add files via upload

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3b97bc4e-660d-48e5-8c66-7daefb7b52e4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using Calculator2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Calculation cal;
+        [TestInitialize]
+        /*
+         Test input: 10, 5
+        */
+        public void Setup() 
+        {
+            cal = new Calculation(10, 5);
+        }
+
+        [TestMethod]
+        public void TestAddOperator()
+        {
+            int expected = 15;
+            int actual = cal.Execute("+");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestSubOperator()
+        {
+            //Output: 5
+            int expected = 5;
+            int actual = cal.Execute("-");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMulOperator()
+        {
+            //Output: 50
+            int expected = 50;
+            int actual = cal.Execute("*");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestDivOperator()
+        {
+            //Output: 2
+            int expected = 2;
+            int actual = cal.Execute("/");
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestDivByZero()
+        {
+            //Output: 2
+            Calculation c = new Calculation(2, 0);
+            c.Execute("/");
+        }
+    }
+}
