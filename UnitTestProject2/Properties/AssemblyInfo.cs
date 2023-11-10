6f0c127a1f9abf22d30d94b77afb7f58bf87201c
Kriss <69229024+Krisspal@users.noreply.github.com>
2023-11-10 12:41:35 +0700

    Add files via upload

---
+++ UnitTestProject2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject2")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("89a486f8-85c2-45f8-bc61-398e99ae8395")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,44 @@
+﻿using Calculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        SimpleEquation cal;
+        [TestMethod]
+        public void THVoNghiem()
+        {
+            /*
+             a,b: 0, 1
+            VN
+            */
+            SimpleEquation s = new SimpleEquation(0, 1);
+            Assert.AreEqual("VN", s.SingleEquation());
+        }
+
+        [TestMethod]
+        public void THVoSoNghiem()
+        {
+            /*
+             a,b: 0, 1
+            VN
+            */
+            SimpleEquation s = new SimpleEquation(0, 0);
+            Assert.AreEqual("VSN", s.SingleEquation());
+        }
+
+        [TestMethod]
+        public void THMotNgiem()
+        {
+            /*
+             a,b: 1, 2
+            VN
+            */
+            SimpleEquation s = new SimpleEquation(2, 1);
+            Assert.AreEqual("-0.5", s.SingleEquation());
+        }
+    }
+}
